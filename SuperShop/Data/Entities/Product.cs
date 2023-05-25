a5019d23766e52502a37eb3b9eeec63cf484f93f
Filipe Baptista <filipebatista.adv@gmail.com>
2023-05-25 11:48:22 +0100

    Modify Products

--- SuperShop/Data/Entities/Product.cs
+++ SuperShop/Data/Entities/Product.cs
@@ -8,6 +8,8 @@ namespace SuperShop.Data.Entities
         [Key] //to define that is a primary key EF
         public int Id { get; set; }
 
+        [Required]
+        [MaxLength(50, ErrorMessage = "The field {0} can contain {1} characters length.")]
         public string Name { get; set; }
 
         [DisplayFormat(DataFormatString = "{0:C2}", ApplyFormatInEditMode = false)] //formats type, if in edit mode doesnt use the format
@@ -17,10 +19,10 @@ namespace SuperShop.Data.Entities
         public string ImageUrl { get; set; }
 
         [Display(Name = "Last Purchase")]
-        public DateTime LastPurchase { get; set; }
+        public DateTime? LastPurchase { get; set; }
 
         [Display(Name = "Last Sale")]
-        public DateTime LastSale { get; set; }
+        public DateTime? LastSale { get; set; }
 
         [Display(Name = "Is Available")]
         public bool IsAvailable { get; set; }
