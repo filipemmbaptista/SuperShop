b74bffd02e2a37f29d87ba77d66c87314da2db76
Filipe Baptista <filipebatista.adv@gmail.com>
2023-05-31 15:25:15 +0100

    Add and Edit Images Images and User on API Products

--- SuperShop/Data/IProductRepository.cs
+++ SuperShop/Data/IProductRepository.cs
@@ -1,8 +1,11 @@
-﻿using SuperShop.Data.Entities;
+﻿using Microsoft.AspNetCore.Mvc;
+using SuperShop.Data.Entities;
+using System.Linq;
 
 namespace SuperShop.Data
 {
     public interface IProductRepository : IGenericRepository<Product>
     {
+        public IQueryable GetAllWithUsers();
     }
 }
