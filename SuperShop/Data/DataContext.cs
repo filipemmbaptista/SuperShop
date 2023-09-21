13a429e493008ed43b72d16e46c83de69c50124e
Filipe Baptista <filipebatista.adv@gmail.com>
2023-09-21 11:46:50 +0100

    Country and Price Changes

--- SuperShop/Data/DataContext.cs
+++ SuperShop/Data/DataContext.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using SuperShop.Data.Entities;
 using System.Linq;
+using System.Reflection.Emit;
 
 namespace SuperShop.Data
 {
@@ -21,6 +22,19 @@ namespace SuperShop.Data
 
         public DataContext(DbContextOptions<DataContext> options) : base(options)
         {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Country>().HasIndex(c => c.Name).IsUnique();
+
+            modelBuilder.Entity<Product>().Property(p => p.Price).HasColumnType("decimal(18,2)");
+            
+            modelBuilder.Entity<OrderDetailTemp>().Property(p => p.Price).HasColumnType("decimal(18,2)");
+
+            modelBuilder.Entity<OrderDetail>().Property(p => p.Price).HasColumnType("decimal(18,2)");
+
+            base.OnModelCreating(modelBuilder);
         }
 
         //Serve para habilitar a regra de apagar em cascata
